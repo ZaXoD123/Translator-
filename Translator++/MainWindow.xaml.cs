fea004e1df96266972ae90660df984157c5b2d71
Zaxod123 <nikulinnikolai@mail.ru>
2018-09-01 19:58:42 +0400

    Добавьте файлы проекта.

---
+++ Translator++/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Translator__;
+
+namespace Translator__
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    ///
+    
+    public partial class MainWindow : Window
+    {
+        string eng = @"`qwertyuiop[]asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+|QWERTYUIOP{}ASDF" + "GHJKL:\"ZXCVBNM<>?";
+        string rus = @"ёйцукенгшщзхъфывапролджэячсмитьбю.Ё!" + "\"№;%:?*()_+/ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,";
+
+        TRANSLATOR tr = new TRANSLATOR();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            ExampleText.Text = tr.translate(rus, eng);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ExampleText.Text = tr.translate(eng,rus);
+        }
+
+        
+    }
+}
---
+++ Translator++/TRANSLATOR.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translator__
+{
+    
+    class TRANSLATOR
+    {
+        
+        int count = 0;
+        [STAThread]
+        public virtual string translate(string eng, string rus)
+        {
+            if (count > 1) { count = 0; return "строка в разных раскладках"; }
+            StringBuilder builder = new StringBuilder(Clipboard.GetText());
+            for (int i = 0; i < builder.Length; i++)
+            {
+                try
+                {
+                    builder.Insert(i, eng[rus.IndexOf(builder[i])].ToString(), 1);
+                    builder.Remove(i + 1, 1);
+                }
+                catch
+                {
+                    if ((rus.IndexOf(builder[i]) == -1)&&(eng.IndexOf(builder[i]) != -1)){ count++; return translate(rus,eng); }
+                    continue;
+                }
+                
+            }
+            count = 0;
+            Clipboard.SetText(builder.ToString());
+            return builder.ToString();
+        }
+        
+    }
+}
